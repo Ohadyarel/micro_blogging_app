583df684cb94311e1350278c25a87e02f562f906
Ohad Yarel <ohadyarel@gmail.com>
2016-04-27 17:35:19 -0400

    adding relations in the models.rb + commenting out the seed file

--- models.rb
+++ models.rb
@@ -1,21 +1,27 @@
 class User < ActiveRecord::Base
-
+	has_many :posts
+	has_many :follows
+	has_many :users, through: :follows
 end
 
 #=========================================
 
 class Post < ActiveRecord::Base
-
+	belongs_to :user
+	has_many :likes
+	has_many :users, through: :likes
 end
 
 #=========================================
 
 class Like < ActiveRecord::Base
-
+	belongs_to :post
+	belongs_to :user
 end
 
 #=========================================
 
 class Follow < ActiveRecord::Base
+	#Still need to figure this part out
 
 end
