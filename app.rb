bc436b329810629bd87d448fd30f683d3c3d1e0e
Juan Pol <jpol0191@gmail.com>
2016-04-29 09:18:33 -0400

    Adding comments to the app.rb

--- app.rb
+++ app.rb
@@ -11,20 +11,25 @@ use Rack::Flash, :sweep => true
 configure(:development){set :database, 'sqlite3:rettiwt.sqlite3'}
 set :sessions, true 
 
+#This function places the session user into a var called @current_user.
 def current_user
   if session[:user_id]
     @current_user = User.find(session[:user_id])
   end
 end
 
+#This is the get for the home page.
 get '/' do 
 	erb :index
 end
 
+#This is the get for the sign up page that contains the sign up form.
 get '/sign_up' do 
 	erb :sign_up
 end
 
+# This is the code that validates the information entered from the signup form
+# then updates the data base with the correct user information.
 post '/sign-up' do
 	if params[:terms] == "true"
 		if params[:login][:password] == params[:repassword]
@@ -45,6 +50,7 @@ post '/sign-up' do
 	end
 end
 
+#This code checks the user log-in information 
 post '/log-in' do
 	@user = User.where(username: params[:username]).first
 	if @user && @user.password == params[:password]
@@ -56,22 +62,25 @@ post '/log-in' do
 	end   
 end
 
+#This code adds a post to the users posts
 post '/post/create' do
 	post = Post.create(params[:post])
 	redirect "/feed"
 end
 
+#This leads the user to the feed. Which shows the last 10 post that were made.
 get '/feed' do
 	@posts = Post.all.slice(Post.all.length - 10, 10)
 	erb :feed
 end 
 
-
+#This will log out the user then direct them to the home page
 get '/log-out' do
 	session.clear
 	redirect '/'
 end
 
+#This leads the user to the profile of the target user.
 get '/profile/:username' do
 	current_user
 	@profile_user = User.where(username: params[:username]).first
@@ -79,12 +88,13 @@ get '/profile/:username' do
 	erb :profile
 end
 
-
+#This lets the sessions user edit their account information
 get '/profile_edit/:username' do
 	current_user
 	erb :profile_edit
 end
 
+#The following post let the user change their info or delete their account.
 post '/edit-fname' do 
 	current_user
 	if params[:fname] != ""
@@ -137,7 +147,9 @@ post '/account-delete' do
 	session.clear
 	redirect '/'
 end
+#===================End of account editing post==============================
 
+#The following post allow the user to follow/unfollow the profile user.
 post '/unfollow' do
 	Follow.find(params[:follow_id]).delete
 	redirect "/profile/#{params[:profile_username]}"
